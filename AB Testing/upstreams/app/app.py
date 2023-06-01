ab98522821c7e49aefbd2c10a79e5c777f210f21
Proxypepe <sasha_golovin_95@bk.ru>
2023-06-01 19:14:47 +0000

    added ab testing. site and upstream examples

---
+++ AB Testing/upstreams/app/app.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+import uuid
+
+app = FastAPI()
+uuid_gen = str(uuid.uuid4())
+
+@app.get("/")
+def read_root():
+    return {"uuid": uuid_gen}
+
+@app.get("/uuid")
+def read_uuid():
+    return {"uuid": uuid_gen}
+
+@app.get("/ping")
+def ping():
+    return {"pong": uuid_gen}
